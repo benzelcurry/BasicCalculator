78e58320438028d94b2044f148d8e2fd8f4d7d89
Benzel <benawhiting@gmail.com>
2025-02-17 13:33:47 -0800

    Update standard to C++20 and add helper function for formatting responses

--- BasicCalculator.cpp
+++ BasicCalculator.cpp
@@ -3,8 +3,34 @@
 
 #include "math.h";
 #include <iostream>
+#include <string>
+#include <format>
+
+std::string answer_str(int num1, int num2, char op)
+{
+    return std::format("The answer of {} {} {}", num1, op, num2);
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello, welcome to your personal calculator!\n";
+
+    std::cout << "Please enter your first number:\n";
+    int x{};
+    std::cin >> x;
+
+    std::cout << "Please enter your second number:\n";
+    int y{};
+    std::cin >> y;
+
+    std::cout << "Please choose the operator you'd like to use:\n";
+    std::cout << "Available operators: [ +, -, *, / ]\n";
+    char op{}; // Holds the operator to be used
+    std::cin >> op;
+
+    switch (op)
+    {
+    case '+':
+
+    }
 }
