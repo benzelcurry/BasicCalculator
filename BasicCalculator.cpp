09ac748946b5a1bf09b2382c4431526082db2b5d
Benzel <benawhiting@gmail.com>
2025-02-17 13:39:54 -0800

    Implement logic for manipulating user input for calculations

--- BasicCalculator.cpp
+++ BasicCalculator.cpp
@@ -8,18 +8,18 @@
 
 std::string answer_str(int num1, int num2, char op)
 {
-    return std::format("The answer of {} {} {}", num1, op, num2);
+    return std::format("The answer of {} {} {} is: ", num1, op, num2);
 }
 
 int main()
 {
     std::cout << "Hello, welcome to your personal calculator!\n";
 
-    std::cout << "Please enter your first number:\n";
+    std::cout << "Please enter your first number (whole numbers only):\n";
     int x{};
     std::cin >> x;
 
-    std::cout << "Please enter your second number:\n";
+    std::cout << "Please enter your second number (whole numbers only):\n";
     int y{};
     std::cin >> y;
 
@@ -31,6 +31,21 @@ int main()
     switch (op)
     {
     case '+':
-
+        std::cout << answer_str(x, y, op) << add(x, y) << '\n';
+        break;
+    case '-':
+        std::cout << answer_str(x, y, op) << subtract(x, y) << '\n';
+        break;
+    case '*':
+        std::cout << answer_str(x, y, op) << multiply(x, y) << '\n';
+        break;
+    case '/':
+        std::cout << answer_str(x, y, op) << divide(x, y) << '\n';
+        break;
+    default:
+        std::cout << "Illegal operator detected. Terminating program.\n";
+        break;
     }
+
+    return 0;
 }
